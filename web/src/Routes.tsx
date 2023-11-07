e35dba9f88d67e4feb5e678c9e6c7d904ec3cd55
Drikus Roor <drikusroor@gmail.com>
2023-11-07 15:09:06 +0100

    feat: Protect private routes and redirect to login if not logged in

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -16,29 +16,31 @@ import { useAuth } from './auth'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Set wrap={DashboardLayout} title="Study Sets" addNewButton={{ to: routes.newStudySet, label: 'New Study Set' }}>
-        <Route path="/study-sets/new" page={StudySetNewStudySetPage} name="newStudySet" />
-        <Route path="/study-sets/{id:Int}/edit" page={StudySetEditStudySetPage} name="editStudySet" />
-        <Route path="/study-sets/{id:Int}" page={StudySetStudySetPage} name="studySet" />
-        <Route path="/study-sets" page={StudySetStudySetsPage} name="studySets" />
-      </Set>
-      <Set wrap={DashboardLayout} title="StudySetDecks">
-        <Route path="/study-set-decks/new" page={StudySetDeckNewStudySetDeckPage} name="newStudySetDeck" />
-        <Route path="/study-set-decks/{id:Int}/edit" page={StudySetDeckEditStudySetDeckPage} name="editStudySetDeck" />
-        <Route path="/study-set-decks/{id:Int}" page={StudySetDeckStudySetDeckPage} name="studySetDeck" />
-        <Route path="/study-set-decks" page={StudySetDeckStudySetDecksPage} name="studySetDecks" />
-      </Set>
-      <Set wrap={DashboardLayout} title="Flashcards">
-        <Route path="/flashcards/new" page={FlashcardNewFlashcardPage} name="newFlashcard" />
-        <Route path="/flashcards/{id:Int}/edit" page={FlashcardEditFlashcardPage} name="editFlashcard" />
-        <Route path="/flashcards/{id:Int}" page={FlashcardFlashcardPage} name="flashcard" />
-        <Route path="/flashcards" page={FlashcardFlashcardsPage} name="flashcards" />
-      </Set>
-      <Set wrap={DashboardLayout} title="Decks" addNewButton={{ to: routes.newDeck, label: 'New Deck' }}>
-        <Route path="/decks/new" page={DeckNewDeckPage} name="newDeck" />
-        <Route path="/decks/{id:Int}/edit" page={DeckEditDeckPage} name="editDeck" />
-        <Route path="/decks/{id:Int}" page={DeckDeckPage} name="deck" />
-        <Route path="/decks" page={DeckDecksPage} name="decks" />
+      <Set private unauthenticated="login">
+        <Set wrap={DashboardLayout} title="Study Sets" addNewButton={{ to: routes.newStudySet, label: 'New Study Set' }}>
+          <Route path="/study-sets/new" page={StudySetNewStudySetPage} name="newStudySet" />
+          <Route path="/study-sets/{id:Int}/edit" page={StudySetEditStudySetPage} name="editStudySet" />
+          <Route path="/study-sets/{id:Int}" page={StudySetStudySetPage} name="studySet" />
+          <Route path="/study-sets" page={StudySetStudySetsPage} name="studySets" />
+        </Set>
+        <Set wrap={DashboardLayout} title="StudySetDecks">
+          <Route path="/study-set-decks/new" page={StudySetDeckNewStudySetDeckPage} name="newStudySetDeck" />
+          <Route path="/study-set-decks/{id:Int}/edit" page={StudySetDeckEditStudySetDeckPage} name="editStudySetDeck" />
+          <Route path="/study-set-decks/{id:Int}" page={StudySetDeckStudySetDeckPage} name="studySetDeck" />
+          <Route path="/study-set-decks" page={StudySetDeckStudySetDecksPage} name="studySetDecks" />
+        </Set>
+        <Set wrap={DashboardLayout} title="Flashcards">
+          <Route path="/flashcards/new" page={FlashcardNewFlashcardPage} name="newFlashcard" />
+          <Route path="/flashcards/{id:Int}/edit" page={FlashcardEditFlashcardPage} name="editFlashcard" />
+          <Route path="/flashcards/{id:Int}" page={FlashcardFlashcardPage} name="flashcard" />
+          <Route path="/flashcards" page={FlashcardFlashcardsPage} name="flashcards" />
+        </Set>
+        <Set wrap={DashboardLayout} title="Decks" addNewButton={{ to: routes.newDeck, label: 'New Deck' }}>
+          <Route path="/decks/new" page={DeckNewDeckPage} name="newDeck" />
+          <Route path="/decks/{id:Int}/edit" page={DeckEditDeckPage} name="editDeck" />
+          <Route path="/decks/{id:Int}" page={DeckDeckPage} name="deck" />
+          <Route path="/decks" page={DeckDecksPage} name="decks" />
+        </Set>
       </Set>
       <Set wrap={DashboardLayout}>
         <Route path="/home" page={HomePage} name="home" />
