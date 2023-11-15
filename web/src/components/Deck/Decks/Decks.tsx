a96f8c984f748df91e6d6a4fa3cf73b6fd1e5e3f
Drikus Roor <drikusroor@gmail.com>
2023-11-15 11:01:30 +0100

    feat: Allow users to add new deck

--- web/src/components/Deck/Decks/Decks.tsx
+++ web/src/components/Deck/Decks/Decks.tsx
@@ -1,3 +1,4 @@
+import { FaPlus, FaServicestack, FaStackpath } from 'react-icons/fa'
 import type { DeleteDeckMutationVariables, FindDecks } from 'types/graphql'
 
 import { Link, routes } from '@redwoodjs/router'
@@ -37,68 +38,81 @@ const DecksList = ({ decks }: FindDecks) => {
   }
 
   return (
-    <div className="rw-segment rw-table-wrapper-responsive bg-sand drop-shadow-2xl">
-      <table className="rw-table">
-        <thead>
-          <tr>
-            <th className="!bg-transparent">Id</th>
-            <th className="!bg-transparent">User id</th>
-            <th className="!bg-transparent">Name</th>
-            <th className="!bg-transparent">Description</th>
-            <th className="!bg-transparent">Flashcards</th>
-            <th className="!bg-transparent">Created at</th>
-            <th className="!bg-transparent">Updated at</th>
-            <th className="!bg-transparent">&nbsp;</th>
-          </tr>
-        </thead>
-        <tbody>
-          {decks.map((deck) => (
-            <tr key={deck.id}>
-              <td className="!bg-transparent">{truncate(deck.id)}</td>
-              <td className="!bg-transparent">{truncate(deck.userId)}</td>
-              <td className="!bg-transparent">{truncate(deck.name)}</td>
-              <td className="!bg-transparent">{truncate(deck.description)}</td>
-              <td className="!bg-transparent">
-                <span
-                  title={deck.flashcards?.length.toString()}
-                  className="rounded-full bg-blue-500 px-2 text-white"
-                >
-                  {truncate(deck.flashcards?.length)}
-                </span>
-              </td>
-              <td className="!bg-transparent">{timeTag(deck.createdAt)}</td>
-              <td className="!bg-transparent">{timeTag(deck.updatedAt)}</td>
-              <td className="!bg-transparent">
-                <nav className="rw-table-actions">
-                  <Link
-                    to={routes.deck({ id: deck.id })}
-                    title={'Show deck ' + deck.id + ' detail'}
-                    className="rw-button rw-button-small"
-                  >
-                    Show
-                  </Link>
-                  <Link
-                    to={routes.editDeck({ id: deck.id })}
-                    title={'Edit deck ' + deck.id}
-                    className="rw-button rw-button-small rw-button-blue"
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    type="button"
-                    title={'Delete deck ' + deck.id}
-                    className="rw-button rw-button-small rw-button-red"
-                    onClick={() => onDeleteClick(deck.id)}
-                  >
-                    Delete
-                  </button>
-                </nav>
-              </td>
+    <>
+      <div className="rw-segment rw-table-wrapper-responsive bg-sand drop-shadow-2xl">
+        <table className="rw-table">
+          <thead>
+            <tr>
+              <th className="!bg-transparent">Id</th>
+              <th className="!bg-transparent">User id</th>
+              <th className="!bg-transparent">Name</th>
+              <th className="!bg-transparent">Description</th>
+              <th className="!bg-transparent">Flashcards</th>
+              <th className="!bg-transparent">Created at</th>
+              <th className="!bg-transparent">Updated at</th>
+              <th className="!bg-transparent">&nbsp;</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {decks.map((deck) => (
+              <tr key={deck.id}>
+                <td className="!bg-transparent">{truncate(deck.id)}</td>
+                <td className="!bg-transparent">{truncate(deck.userId)}</td>
+                <td className="!bg-transparent">{truncate(deck.name)}</td>
+                <td className="!bg-transparent">
+                  {truncate(deck.description)}
+                </td>
+                <td className="!bg-transparent">
+                  <span
+                    title={deck.flashcards?.length.toString()}
+                    className="rounded-full bg-blue-500 px-2 text-white"
+                  >
+                    {truncate(deck.flashcards?.length)}
+                  </span>
+                </td>
+                <td className="!bg-transparent">{timeTag(deck.createdAt)}</td>
+                <td className="!bg-transparent">{timeTag(deck.updatedAt)}</td>
+                <td className="!bg-transparent">
+                  <nav className="rw-table-actions">
+                    <Link
+                      to={routes.deck({ id: deck.id })}
+                      title={'Show deck ' + deck.id + ' detail'}
+                      className="rw-button rw-button-small"
+                    >
+                      Show
+                    </Link>
+                    <Link
+                      to={routes.editDeck({ id: deck.id })}
+                      title={'Edit deck ' + deck.id}
+                      className="rw-button rw-button-small rw-button-blue"
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      type="button"
+                      title={'Delete deck ' + deck.id}
+                      className="rw-button rw-button-small rw-button-red"
+                      onClick={() => onDeleteClick(deck.id)}
+                    >
+                      Delete
+                    </button>
+                  </nav>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Link
+        to={routes.newDeck()}
+        className="mt-5 inline-block rounded-lg bg-forest p-2 text-sand drop-shadow transition-colors hover:text-salmon"
+      >
+        <div className="flex flex-row items-center gap-2">
+          <div>Add new deck</div>
+          <FaPlus />
+        </div>
+      </Link>
+    </>
   )
 }
 
