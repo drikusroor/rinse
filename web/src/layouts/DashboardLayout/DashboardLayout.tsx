756dd6b04f51b0cdc5824626589b1b78e9ac1e31
Drikus Roor <drikusroor@gmail.com>
2023-11-23 16:00:56 +0100

    feat: Add Avatar component

--- web/src/layouts/DashboardLayout/DashboardLayout.tsx
+++ web/src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,9 +1,10 @@
-import { FaPlusCircle, FaUserCircle } from 'react-icons/fa'
+import { FaPlusCircle } from 'react-icons/fa'
 
 import { Link, routes } from '@redwoodjs/router'
 import { Toaster } from '@redwoodjs/web/dist/toast'
 
 import { useAuth } from 'src/auth'
+import Avatar from 'src/components/Avatar/Avatar'
 
 interface AddNewButton {
   to: () => string
@@ -79,24 +80,7 @@ const DashboardLayout = ({
             <div className="flex items-center md:space-x-6">
               <div className="relative flex-shrink-0">
                 <div className="group inline-block text-left">
-                  <button
-                    type="button"
-                    className="flex rounded-full bg-sand text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    id="user-menu"
-                    aria-expanded="false"
-                    aria-haspopup="true"
-                    title={currentUser?.email}
-                    aria-label={currentUser?.email}
-                  >
-                    <span className="sr-only">Open user menu</span>
-                    {!currentUser ? (
-                      <FaUserCircle className="h-8 w-8 text-gray-500" />
-                    ) : (
-                      <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gradient-to-br from-forest to-salmon text-white">
-                        {currentUser?.email?.charAt(0).toUpperCase()}
-                      </div>
-                    )}
-                  </button>
+                  <Avatar user={currentUser}></Avatar>
                   <div className="absolute right-0 z-10 hidden group-hover:block">
                     <ul
                       className="mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5"
