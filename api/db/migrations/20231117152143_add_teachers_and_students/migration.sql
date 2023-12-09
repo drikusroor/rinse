36f8ee55a74457371ef8a4be1276f22982b55983
Drikus Roor <drikusroor@gmail.com>
2023-12-09 20:32:08 +0100

    db: Remove duplicate firstName & lastName columns

--- api/db/migrations/20231117152143_add_teachers_and_students/migration.sql
+++ api/db/migrations/20231117152143_add_teachers_and_students/migration.sql
@@ -6,8 +6,6 @@
 */
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "firstName" TEXT,
-ADD COLUMN     "lastName" TEXT,
 ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
 
 -- CreateTable
